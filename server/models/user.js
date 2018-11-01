a7a777f924affd416bfe78977e9f4611b47a96ef
robus <kbhatt365@gmail.com>
2018-11-01 19:13:27 +0530

    mongodb connection setup

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,11 @@
+import { ModuleExportMetadata } from '@angular/compiler-cli/src/metadata';
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
     email: String,
-    password: String
+    password: String,
+    isAdmin: Boolean
 })
+
+module.exports = mongoose.model('user', userSchema,'users')
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,22 @@
-import { Router } from '@angular/router';
 const express = require('express')
 const router = express.Router()
+const obj = require('./db.json')
 
-router.get('/', (req,res)  => {
-    res.send('from API Routes')
+const mongoose = require('mongoose')
+const dbUrl = obj.uri
+console.log(dbUrl)
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, err => {
+  if (err) {
+    console.error('Error!', + err)
+  } else {
+    console.log('db is connected')
+  }
+})
+
+router.get('/', (req, res) => {
+  res.send('from API Routes')
 })
 
 module.exports = router
+
