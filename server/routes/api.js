13de99878dd163d7cb854a34824a973941912872
teslaji <kbhatt365@gmail.com>
2018-11-02 17:40:05 +0530

    Login & Register API endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,23 +1,57 @@
 const express = require('express')
 const router = express.Router()
 const obj = require('./db.json')
-
+const User = require('./server/models/user')
 const mongoose = require('mongoose')
 const dbUrl = obj.uri
 console.log(dbUrl)
 
-mongoose.connect(dbUrl, { useNewUrlParser: true } , err => {
-  if(err){
-    console.error ('Error!' + err)
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true
+}, err => {
+  if (err) {
+    console.error('Error!' + err)
   } else {
     console.log('Connected to MongoDb')
   }
 })
 
-  
+
 router.get('/', (req, res) => {
   res.send('from API Routes')
 })
 
-module.exports = router
+router.post('/register', (req, res) => {
+  let userData = req.body
+  let user = new User(userData)
+  user.save((err, registeredUser) => {
+    if (err) {
+      console.log(error)
+    } else {
+      res.status(200).send(registeredUser)
+    }
+  })
 
+})
+
+router.post('/login', (req, res) => {
+  let userData = req.body
+  User.findone({
+    email: userData.email
+  }, (error, user) => {
+    if (error) {
+      console.log(error)
+    } else {
+      if (!user) {
+        res.status(401).send('Invalid Email')
+      } else
+      if (user.password !== userData.password) {
+        res.status(401).send('Invalid Password')
+      } else {
+        res.status(200).send(user)
+      }
+    }
+  })
+})
+
+module.exports = router
