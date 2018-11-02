ccd17770694f57c97e018f49808ddd49c0e084cb
teslaji <kbhatt365@gmail.com>
2018-11-02 17:18:59 +0530

    server fix mlabs

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,14 +6,15 @@ const mongoose = require('mongoose')
 const dbUrl = obj.uri
 console.log(dbUrl)
 
-mongoose.connect(dbUrl, {useNewUrlParser: true}, err => {
-  if (err) {
-    console.error('Error!', + err)
+mongoose.connect(dbUrl, { useNewUrlParser: true } , err => {
+  if(err){
+    console.error ('Error!' + err)
   } else {
-    console.log('db is connected')
+    console.log('Connected to MongoDb')
   }
 })
 
+  
 router.get('/', (req, res) => {
   res.send('from API Routes')
 })
--- server/server.js
+++ server/server.js
@@ -13,5 +13,5 @@ app.get('/', (req,res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(' server running on host: ', + PORT )
+    console.log('server running on host:' + PORT)
 })
