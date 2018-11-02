c8ae351491b8e3dec24de71d417ed258e15ddcc3
teslaji <kbhatt365@gmail.com>
2018-11-02 19:11:42 +0530

    CORS added

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
+    fullname: String,
     email: String,
     password: String,
     isAdmin: Boolean
