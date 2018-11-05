e7d56c1f536480ee5624f79f18098e9603c4deda
teslaji <kbhatt365@gmail.com>
2018-11-05 14:38:47 +0530

    server side token verification

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,6 +18,21 @@ mongoose.connect(dbUrl, {
   }
 })
 
+function verifyToken(req, res, next){
+  if (!req.headers.authorization) {
+    return res.status(401).send('Unauthorized request')  
+  }
+  let token = req.headers.authorization.split(' ')[1]
+  if (token === 'null') {
+    return res.status(401).send('Unauthorized request') 
+  }
+  let payload = jwt.verify(token, 'secretKey')
+  if(!payload){
+    return res.status(401).send('Unauthorized request') 
+  }
+  req.userID =payload.subject
+  next()
+}
 
 router.get('/', (req, res) => {
   res.send('from API Routes')
